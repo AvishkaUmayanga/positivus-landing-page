b001349f1bf511e2925382549624614a28016cb4
AvishkaUmayanga <avishkas97@gmail.com>
2024-08-25 17:56:17 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import WorkingProcess from "@/components/working process/WorkingProcess";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-20 px-12 py-3 2xl:px-20 max-md:px-5">
+    <main className="flex flex-col gap-36 px-12 py-3 2xl:px-20 max-md:px-5">
       <HeroSection />
       <ServicesSection />
       <FreeProposal />
